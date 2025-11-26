c39e29e93f96c2bf29bc08508ef8f9f096441649
HHPapegowda <harish.h.papegowda@gmail.com>
2025-11-26 12:13:14 -0600

    Create streamlit_app.py
    Copied from my Custom Smoothies SiS app

---
+++ streamlit_app.py
@@ -0,0 +1,35 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col, when_matched
+
+
+# Write directly to the app
+st.title(f":cup_with_straw: Pending Smoothie Orders! :cup_with_straw:")
+st.write("Orders that need to be filled.")
+
+name_on_order = st.text_input('Name on Smoothie')
+st.write("The name on your Smoothie will be:", name_on_order)
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
+editable_df = st.data_editor(my_dataframe)
+
+if my_dataframe:
+    editable_df = st.data_editor(my_dataframe)
+    submitted = st.button('Submit')
+    if Submitted:
+        
+        og_dataset = session.table("smoothies.public.orders")
+        edited_dataset = session.create_dataframe(editable_df)
+
+        try:
+            og_dataset.merge(edited_dataset
+                     , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
+                     , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                    )
+            st.success('Someone clicked the button', icon = '👍')
+        except:
+            st.write('Somrthing went wrong')
+    else:
+        st.success('There are not pending ordrs right now', icon = '👍')
